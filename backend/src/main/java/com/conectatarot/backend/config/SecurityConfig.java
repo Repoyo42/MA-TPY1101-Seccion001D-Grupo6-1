203274cd4ad6425ed831430021228ab781d02968
Germanwithpants <ric.gaete@duocuc.cl>
2026-06-11 22:56:00 -0400

    fix: permitir endpoint /api/auth/google sin autenticacion

--- backend/src/main/java/com/conectatarot/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/conectatarot/backend/config/SecurityConfig.java
@@ -45,9 +45,10 @@ public class SecurityConfig {
                 )
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login",
-                                        "/api/usuarios"
-                        )
+        	        .requestMatchers("/api/auth/login",
+        	       		 "/api/auth/google",
+		       		 "/api/usuarios"
+                )
                         .permitAll()
                         .anyRequest()
                         .authenticated()
